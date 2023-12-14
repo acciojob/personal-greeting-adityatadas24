809aeafe74bdc2c59fe4d35808b502c76ac7b968
adityatadas24 <adityatadas2000@gmail.com>
2023-12-14 19:55:03 +0530

    submitting code-Thu Dec 14 2023 19:55:02 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
- const [text, setText]= useState("");
+ const [text, setText]= useState("Hello");
 
  function inputs(e){
   setText(e.target.value)
